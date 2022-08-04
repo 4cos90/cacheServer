a797024e9c43404ab0f86eb6526143c3a5b5efa1
4cos90 <910508524@qq.com>
2022-08-04 23:01:47 +0800

    first commit

---
+++ cacheServer/cacheServer.go
@@ -0,0 +1,75 @@
+package cacheServer
+
+import (
+	"time"
+)
+
+type messageCache struct {
+	receiver string
+	message  string
+	time     time.Time
+}
+
+type cache struct {
+	Name    string
+	Cache   []messageCache
+	Timeout time.Duration
+}
+
+type CacheServer interface {
+	Get(key string) []messageCache
+	Set(receiver string, message string, time time.Time) error
+	GetAll() []messageCache
+}
+
+func (s *cache) GetAll() []messageCache {
+	for startindex := 0; startindex < len(s.Cache); startindex++ {
+		if time.Now().Sub(s.Cache[startindex].time) < s.Timeout {
+			s.Cache = s.Cache[startindex:]
+			break
+		} else if startindex == (len(s.Cache) - 1) {
+			s.Cache = make([]messageCache, 0)
+		}
+	}
+	return s.Cache
+}
+
+func (s *cache) Get(key string) []messageCache {
+	for startindex := 0; startindex < len(s.Cache); startindex++ {
+		if time.Now().Sub(s.Cache[startindex].time) < s.Timeout {
+			s.Cache = s.Cache[startindex:]
+			break
+		} else if startindex == (len(s.Cache) - 1) {
+			s.Cache = make([]messageCache, 0)
+		}
+	}
+	rlt := make([]messageCache, 1)
+	newCache := make([]messageCache, 1)
+	for i := 0; i < len(s.Cache); i++ {
+		if s.Cache[i].receiver == key {
+			rlt = append(rlt, s.Cache[i])
+		} else {
+			newCache = append(newCache, s.Cache[i])
+		}
+	}
+	s.Cache = newCache
+	return rlt
+}
+
+func (s *cache) Set(receiver string, message string, time time.Time) error {
+	value := messageCache{
+		receiver: receiver,
+		message:  message,
+		time:     time,
+	}
+	s.Cache = append(s.Cache, value)
+	return nil
+}
+
+func NewCache(name string, timeout time.Duration) CacheServer {
+	return &cache{
+		Name:    name,
+		Cache:   make([]messageCache, 1),
+		Timeout: timeout,
+	}
+}
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"cacheServer/cacheServer"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func main() {
+	MessageCache := cacheServer.NewCache("MessageCache", time.Second*5)
+	go Start(":8080", MessageCache)
+	go MockWebSocketMessage(MessageCache)
+	select {}
+}
+
+func MockWebSocketMessage(cache cacheServer.CacheServer) {
+	rand.Seed(time.Now().UnixNano())
+	var i int = 0
+	for {
+		time.Sleep(time.Millisecond * time.Duration(rand.Intn(1000)))
+		var receiver string
+		if i%2 == 0 {
+			receiver = "HYC"
+		} else {
+			receiver = "ZMN"
+		}
+		message := "send message " + strconv.Itoa(i) + " times"
+		cache.Set(receiver, message, time.Now())
+		i = i + 1
+	}
+}
+
+func Start(Port string, cache cacheServer.CacheServer) error {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", health)
+	mux.HandleFunc("/syncCache", syncCache(cache))
+	mux.HandleFunc("/getCache", getCache(cache))
+	svr := &http.Server{Addr: Port, Handler: mux}
+	err := svr.ListenAndServe()
+	return err
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "server work")
+}
+
+func getCache(cache cacheServer.CacheServer) func(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("getCache Success\n")
+	return func(w http.ResponseWriter, r *http.Request) {
+		key := GetUrlArg(r, "key")
+		fmt.Fprintf(w, "Cache Key:%s,%v", key, cache.Get(key))
+	}
+}
+
+func syncCache(cache cacheServer.CacheServer) func(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("syncCache Success\n")
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "All Cache:%v \n", cache.GetAll())
+	}
+}
+
+func GetUrlArg(r *http.Request, name string) string {
+	var arg string
+	values := r.URL.Query()
+	arg = values.Get(name)
+	return arg
+}
